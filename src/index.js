b1090734cb05a486090a9b8047599fa7595127b5
Vyalov Vadim <vyalov.vadim@gmail.com>
2023-05-19 17:33:59 +0300

    FIX-suspense
    bag lazy+RTKQ

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-//import { persistor, store } from './redux/store';
-//import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { store } from './redux/store';
 import './index.css';
 import './fonts/Montserrat-Regular.ttf';
@@ -13,12 +13,12 @@ import './fonts/Montserrat-Medium.ttf';
 import './fonts/Montserrat-SemiBold.ttf';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        {/* <PersistGate loading={null} persistor={persistor}> */}
-        <App />
-        {/* </PersistGate> */}
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,13 +1,13 @@
 //import Loader from 'components/Loader';
-import Loader from "../../components/Loader/Loader";
+// import Loader from '../../components/Loader/Loader';
 import {
   Container,
   Header,
   MenuLink,
-} from "../../components/SharedLayout/SharedLayout";
-import { Suspense } from "react";
-import { Outlet } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+} from '../../components/SharedLayout/SharedLayout';
+// import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 const Layout = () => {
   return (
@@ -18,10 +18,11 @@ const Layout = () => {
         </MenuLink>
         <MenuLink to="/tweets">Tweets</MenuLink>
       </Header>
+
       <main>
-        <Suspense fallback={<Loader />}>
-          <Outlet />
-        </Suspense>
+        {/* <Suspense fallback={null}> */}
+        <Outlet />
+        {/* </Suspense> */}
         <ToastContainer />
       </main>
     </Container>
